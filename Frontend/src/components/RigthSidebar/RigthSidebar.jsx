24bd13d08988df3af375638a1961996b6c8d3d4e
Naveen Kumawat <naveendimond77@gmail.com>
2025-03-30 22:34:15 +0530

    Right Sidebar ChatBox UI Complete ( User Profile And Profile Media List )

--- Frontend/src/components/RigthSidebar/RigthSidebar.jsx
+++ Frontend/src/components/RigthSidebar/RigthSidebar.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import './RightSidebar.css'
-
+import React from "react";
+import "./RightSidebar.css";
+import assets from "../../assets/assets";
 
 const RigthSidebar = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Naveen<img src={assets.green_dot} className="dot" alt="dot" />
+        </h3>
+        <p>Hey , My Name is Naveen</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+    </div>
+  );
+};
 
-export default RigthSidebar
+export default RigthSidebar;
